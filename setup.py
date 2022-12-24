22d8e71dc421146d3e83f2aca0bfcd4900bb3b0a
Brian Staber <brian.staber@gmail.com>
2022-12-24 16:59:07 +0100

    updated info

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="frerojax",
     version="0.1",
     packages=["frerojax"],
-    description="Tutorial for implemented Gaussian process regression from scratch with JAX!",
-    author="Safran Group",
+    description="Just trying to implement a basic Gaussian process regression with JAX",
+    author="Brian Staber",
     author_email="frero.jax@gmail.com",
     include_package_data=True
 )
